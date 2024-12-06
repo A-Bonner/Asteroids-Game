83e1ce47eef6d00c995c7f3a243fd5d61787d856
A-Bonner <aidanbonner140202@gmail.com>
2024-12-05 22:53:59 -0500

    reverted to functional game

--- game.py
+++ game.py
@@ -5,17 +5,15 @@ from ship import Ship
 from bullet import Bullet
 
 pygame.init()
+
 window = pygame.display.set_mode()
+clock = pygame.time.Clock()
 dimensions = pygame.display.get_window_size()
 score = 0
 fontSmall = pygame.font.Font(None, 24)
 fontBig = pygame.font.Font(None, 96)
 
 running = True
-start = True
-playing = False
-paused = False
-done = False
 
 stars = []
 for i in range(0, 200):
@@ -40,87 +38,69 @@ ship.add(Ship("Ship.png", 10, dimensions[0]/2, dimensions[1]/2))
 move = [0, 0]
 
 while running:
-    if start:
-        startText = fontBig.render("PRESS 'SPACE' TO START", True, 'gray91')
-        window.blit(startText, (dimensions[0]/2 - startText.get_width()/2, dimensions[1]/2 - startText.get_height()/2))
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_SPACE]:
-            print('o_O')
-            if pygame.K_SPACE not in pressed:
-                print('UwU')
-                pressed.append(pygame.K_SPACE)
-        else:
-            print('-_-')
-            if pygame.K_SPACE in pressed:
-                print('OwO')
-                pressed.remove(pygame.K_SPACE)
-                playing = True
-                start = False
-        pygame.display.flip()
+    window.fill('black')
+    for i in stars:
+        pygame.draw.circle(window, 'white', i[0], i[1])
 
-    if playing:
-        window.fill('black')
-        for i in stars:
-            pygame.draw.circle(window, 'white', i[0], i[1])
-        ship.draw(window)
-        ship.update(move, dimensions)
-        move = [0, 0]
+    ship.draw(window)
+    ship.update(move, dimensions)
+    move = [0, 0]
 
-        bullets.draw(window)
-        bullets.update()
-        for bullet in bullets.sprites():
-            for rock in asteroids.sprites():
-                if pygame.sprite.collide_mask(bullet, rock) != None:
-                    rock.update(True)
-                    if rock.health == 0:
-                        rock.destroy(random.randint(0, dimensions[0]), random.randint(-1000, -49))
-                        score += rock.size
-                    bullet.kill()
-
-        asteroids.update(False)
-        asteroids.draw(window)
+    bullets.draw(window)
+    bullets.update()
+    for bullet in bullets.sprites():
         for rock in asteroids.sprites():
-            rock.offscreen(dimensions[1], random.randint(0, dimensions[0]), random.randint(-1000, -49))
-            if pygame.sprite.collide_mask(rock, ship.sprite) != None:
-                rock.relocate(random.randint(0, dimensions[0]), random.randint(-1000, -49))
-                remaining = ship.sprite.hit()
-                if remaining == 0:
-                    done = True
-                    playing = False
+            if pygame.sprite.collide_mask(bullet, rock) != None:
+                rock.update(True)
+                if rock.health == 0:
+                    rock.destroy(random.randint(0, dimensions[0]), random.randint(-1000, -49))
+                    score += rock.size
+                bullet.kill()
 
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_UP]:
-            move[1] = -2
-        if keys[pygame.K_DOWN]:
-            move[1] = 2
-        if keys[pygame.K_RIGHT]:
-            move[0] = 2
-        if keys[pygame.K_LEFT]:
-            move[0] = -2
 
-        if keys[pygame.K_SPACE]:
-            if pygame.K_SPACE not in pressed:
-                pressed.append(pygame.K_SPACE)
-        else:
-            if pygame.K_SPACE in pressed:
-                pressed.remove(pygame.K_SPACE)
-                bullets.add(Bullet("Bullet.png", ship.sprite.getPos()[0], ship.sprite.getPos()[1]))
 
-        if keys[pygame.K_ESCAPE]:
-            done = True
-            playing = False
+    asteroids.update(False)
+    asteroids.draw(window)
+    for rock in asteroids.sprites():
+        rock.offscreen(dimensions[1], random.randint(0, dimensions[0]), random.randint(-1000, -49))
+        if pygame.sprite.collide_mask(rock, ship.sprite) != None:
+            rock.relocate(random.randint(0, dimensions[0]), random.randint(-1000, -49))
+            remaining = ship.sprite.hit()
+            if remaining == 0:
+                running = False
 
-        if score >= 50:
-            for rock in asteroids.sprites():
-                rock.speedUp(1)
-            ship.sprite.health += 1
-            score = 0
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        move[1] = -2
+    if keys[pygame.K_DOWN]:
+        move[1] = 2
+    if keys[pygame.K_RIGHT]:
+        move[0] = 2
+    if keys[pygame.K_LEFT]:
+        move[0] = -2
 
-        hpText = fontSmall.render(("HP: " + str(ship.sprite.health)), True, "white")
-        window.blit(hpText, (10, 10))
+    if keys[pygame.K_SPACE]:
+        if pygame.K_SPACE not in pressed:
+            pressed.append(pygame.K_SPACE)
+    else:
+        if pygame.K_SPACE in pressed:
+            pressed.remove(pygame.K_SPACE)
+            bullets.add(Bullet("Bullet.png", ship.sprite.getPos()[0], ship.sprite.getPos()[1]))
 
-        pygame.display.flip()
-
-    if done:
-        done = False
+    if keys[pygame.K_ESCAPE]:
         running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if score >= 50:
+        for rock in asteroids.sprites():
+            rock.speedUp(1)
+        ship.sprite.health += 1
+        score = 0
+
+    hpText = fontSmall.render(("HP: " + str(ship.sprite.health)), True, "white")
+    window.blit(hpText, (10, 10))
+
+    pygame.display.flip()
+
